7e6015e8caf79066892eb7b10621f2dee3ae22a5
albambg07 <albambg07@educastur.es>
2023-04-30 15:06:30 +0200

    tengo que cambiar de rama jeje

---
+++ resources/js/Components/LogoContainer.jsx
@@ -0,0 +1,14 @@
+export default function LogoContainer() {
+    return (
+        <>
+        {/* <style>
+            div {
+                backgroundImage: url()
+            }
+        </style> */}
+        <div class="w-screen aspect-[4/3]">
+            
+        </div>
+        </>
+    )
+}
